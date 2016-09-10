72d1f31eb65705ec92e6e015dc030a13edfa0efa
tanphatsolution <itshare.asia@gmail.com>
2016-09-10 08:38:43 +0700

    themekho

--- resources/views/backend/warehouse/index.blade.php
+++ resources/views/backend/warehouse/index.blade.php
@@ -1,71 +1,110 @@
 @extends('layouts.backend')
 
+@push('prestyles')
+    {{ HTML::style("assets/css/backend/checkbox-radiobox.css") }}
+    {{ HTML::style("assets/css/backend/promtions/promotion.css") }}
+@endpush
+
 @section('page-content')
-    <div id="RoomsController">
-        @include('backend.warehouse._form')
-        
-        <div id="content">
-            <div class="container-fluid">
-                <h3>Quản lý kho hàng</h3>
-                <div class="row">
-                    <div class="col-xs-12">
-                        <div class="widget">
-                            <div class="widget-heading">
-                                <h1 class="title text-uppercase">Danh kho hàng</h1>
-                                <a href="javascript:;" class="toggle-content" references="#providerList" display="1">
-                                    <i class="fa fa-angle-down"></i>
-                                </a>
+<div id="PositionsController">
+    
+    @include('backend.warehouse._form')
+
+    <div id="content">
+        <div class="container-fluid">
+            <h3>
+                Quản lý kho
+                <a href="#newProvider" role="button" class="btn btn-success pull-right" data-toggle="modal">
+                    <i class="fa fa-plus"></i> Thêm mới kho
+                </a>
+            </h3>
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="widget">
+                        <div class="widget-heading">
+                            <h1 class="title text-uppercase">Danh sách</h1>
+                            <a href="javascript:;" class="toggle-content" references="#providerList" display="1">
+                                <i class="fa fa-angle-down"></i>
+                            </a>
+                        </div>
+                        
+                        <div id="providerList">
+                        
+                            <div class="widget-content">
+                                <div class="table-responsive">
+                                    <table class="table table-condensed table-default table-bordered table-hover" id="table-index">
+                                        <thead>
+                                            <tr class="active">
+                                                <th width="80" class="text-center">Mã</th>
+                                                <th>Tên kho hàng</th>
+                                                <th width="300">Tên chủ kho</th>
+                                                <th width="300">Thuộc chi nhánh</th>
+                                                <th width="80" class="text-center">Thao tác</th>
+                                            </tr>
+                                        </thead>
+
+                                        <tbody>
+                                            <tr style="    background: #e3eff1;">
+                                                <td><input type="" name="" class="event-code" /></td>
+                                                <td><input type="" name="" class="form-control input-sm" /></td>
+                                                <td>
+                                                    <select class="form-control input-sm">
+                                                        <option>Chỉ chọn 1</option>
+                                                    </select>
+                                                </td>
+                                                <td>
+                                                    <select class="form-control input-sm">
+                                                        <option>Cho chọn nhiều chi nhánh</option>
+                                                    </select>
+                                                </td>
+                                                <td class="text-center">
+                                                    <button type="button" class="btn btn-info btn-filter">
+                                                        <span class="glyphicon glyphicon-search"></span> Tìm kiếm
+                                                    </button>
+
+                                                    <button type="button" class="btn btn-danger btn-filter">
+                                                        <span class="glyphicon glyphicon glyphicon-ban-circle"></span> Reset
+                                                    </button>
+                                                </td>
+                                            </tr>
+
+                                            @for($i = 1; $i <= 3; $i ++)
+                                            <tr>
+                                                <td>ABCXYZ</td>
+                                                <td>Get up to 50% off on Candle hotders</td>
+                                                <td>15/11/2014 00h00</td>
+                                                <td>20/11/2014 00h00</td>
+                                                <td class="text-center">
+                                                    <a href="#newRoom"
+                                                        title="Sửa"
+                                                        class="btn-icon label-edit"
+                                                        data-toggle="modal"
+                                                        v-on="click: getUserProfile()">
+                                                        <span class="glyphicon glyphicon-edit"></span>
+                                                    </a>
+
+                                                    <a href="javascript:void(0);" title="Xóa" 
+                                                        class="btn-icon label-delete"
+                                                        v-on:click="destroy(room.id, room)">
+                                                        <span class="glyphicon glyphicon-remove-circle"></span>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            @endfor
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
-                            
-                            <div id="providerList">
-                            @include('backend.warehouse._tool')
-                                <div class="widget-content">
-                                    <div class="table-responsive">
-                                        <table class="table table-condensed table-default table-bordered table-hover" id="table-index">
-                                            <thead>
-                                                <tr class="active">
-                                                    <th style="display:none">ID</th>
-                                                    <th width="100">Mã nhóm</th>
-                                                    <th>Tên kho hàng</th>
-                                                    <th>Tên chủ kho</th>
-                                                    <th>Thuộc chi nhánh</th>
-                                                    <th width="80" class="text-center">Thao tác</th>
-                                                </tr>
-                                            </thead>
-
-                                            <tbody>
-                                                @for($i = 1; $i < 15; $i ++)
-                                                    <tr>
-                                                        <td>CUPA {{ $i }}</td>
-                                                        <td>MANA {{ $i }}</td>
-                                                        <td>MANA {{ $i }}</td>
-                                                        <td>Hà Nội</td>
-                                                        <td class="text-center">
-                                                            <a href="#newProduct" title="Sửa thông tin" class="btn-icon label-edit" data-toggle="modal">
-                                                                <span class="glyphicon glyphicon-edit"></span>
-                                                            </a>
-
-                                                            <a href="#" title="Xóa" class="btn-icon label-delete">
-                                                                <span class="glyphicon glyphicon-remove-circle"></span>
-                                                            </a>
-                                                        </td>
-                                                    </tr>
-                                                @endfor
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                                <div class="widget-footer">
-                                    <div class="text-right">
-                                        <ul class="pagination">
-                                            <li class="active"><a href="#">1</a></li>
-                                            <li><a href="#">2</a></li>
-                                            <li><a href="#">3</a></li>
-                                            <li><a href="#">4</a></li>
-                                            <li><a href="#">5</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
+                        </div>
+                        <div class="widget-footer">
+                            <div class="text-right">
+                                <ul class="pagination">
+                                    <li class="active"><a href="#">1</a></li>
+                                    <li><a href="#">2</a></li>
+                                    <li><a href="#">3</a></li>
+                                    <li><a href="#">4</a></li>
+                                    <li><a href="#">5</a></li>
+                                </ul>
                             </div>
                         </div>
                     </div>
@@ -73,4 +112,5 @@
             </div>
         </div>
     </div>
+</div>
 @endsection
