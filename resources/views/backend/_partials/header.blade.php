c23e5c0cc315ce60bef598c531446e73bb4f6022
tanphatsolution <itshare.asia@gmail.com>
2016-09-14 12:00:57 +0700

    templateNotification

--- resources/views/backend/_partials/header.blade.php
+++ resources/views/backend/_partials/header.blade.php
@@ -15,36 +15,79 @@
 
         <nav class="menu pull-right" references="menu-system">
             <ul>
-                <li>
-                    <a href="{{ route('customers.index') }}"  title="Khách hàng">
-                    <i class="glyphicon glyphicon-user"></i>&nbsp; Khách hàng</a>
-                </li>
 
-                <li>
-                    <a href="#"  title="Kinh doanh">
-                    <i class="glyphicon glyphicon-bullhorn"></i>&nbsp; Kinh doanh</a>
-                </li>
-
-                <li><a href="{{ route('quotations.create') }}">Lập báo giá <i class='glyphicon glyphicon-list-alt'></i></a></li>
-
-                <li>
-                    <a href="{{ route('reports.index') }}">Báo cáo - Thống kê
+                <li class="dropdown">
+                    <a href="{{ route('reports.index') }}"> Báo cáo - Thống kê
                     <i class='glyphicon glyphicon-bullhorn'></i></a>
                 </li>
 
-                <li><a href="{{ route('promotions.index') }}">Khuyến mãi - Tích điểm <i class="glyphicon glyphicon-usd"></i></a></li>
-                <li><a href="#">Phiếu thu <i class="glyphicon glyphicon-equalizer"></i></a></li>
-                <li><a href="#">Phiếu chi <i class="glyphicon glyphicon-equalizer"></i></a></li>
-
                 <li class="dropdown">
                     <a href="javascript::void(0)" data-toggle="dropdown" class="dropdown-toggle" aria-expanded="false">
-                        Hệ thống<span class="glyphicon glyphicon-cog"></span>
+                        Hệ thống <span class="glyphicon glyphicon-cog"></span>
                     </a>
                     <ul class="dropdown-menu submenu">
                         <li><a href="/user"><i class="glyphicon glyphicon-user"></i> Quản lý người dùng</a></li>
                         <li><a href="{{ route('roles.index') }}"><i class="glyphicon glyphicon-lock"> </i> Quản lý nhóm quyền</a></li>
                         <li><a href="{{ route('permissions.index') }}"><i class="glyphicon glyphicon-lock"> </i> Quản lý quyền</a></li>
+                        <li><a href="{{ route('promotions.index') }}"><i class="glyphicon glyphicon-usd"></i> Khuyến mãi - Tích điểm</a></li>
                         <li><a href=""><i class="glyphicon glyphicon-wrench"></i> Cấu hình hệ thống</a></li>
+                    </ul>
+                </li>
+
+                <li class="dropdown" id="notification">
+                    <a id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="/page.html">
+                        <span class="glyphicon glyphicon-bell notification-icon"></span>
+                        <span class="badge notification-mes">3</span>
+                    </a>
+                        
+                    <ul class="dropdown-menu notifications" role="menu" aria-labelledby="dLabel">
+                        
+                        <div class="notification-heading">
+                            <h4 class="menu-title">Thông báo</h4>
+                            <h4 class="menu-title pull-right">
+                                <a href='#' title='Xem tất cả'>Xem tất cả</a>
+                                <i class="glyphicon glyphicon-circle-arrow-right"></i>
+                            </h4>
+                        </div>
+                        <li class="divider"></li>
+
+                        <div class="notifications-wrapper">
+                            <a class="content" href="#">
+                                <div class="notification-item notification-item-new">
+                                    <h4 class="item-title">
+                                        Phiếu yêu cầu mua hàng của bạn đã được duyệt bởi <b>Phạm Kỳ Khôi</b>
+                                    </h4>
+                                    <p><i class="fa fa-clock-o" aria-hidden="true"></i> 1 phút trước</p>
+                                </div>
+                                <div class="notification-item notification-item-new">
+                                    <h4 class="item-title">
+                                        Phiếu yêu cầu mua hàng của bạn đã được duyệt bởi <b>Phạm Kỳ Khôi</b>
+                                    </h4>
+                                    <p><i class="fa fa-clock-o" aria-hidden="true"></i> 1 phút trước</p>
+                                </div>
+                                <div class="notification-item notification-item-new">
+                                    <h4 class="item-title">
+                                        Phiếu yêu cầu mua hàng của bạn đã được duyệt bởi <b>Phạm Kỳ Khôi</b>
+                                    </h4>
+                                    <p><i class="fa fa-clock-o" aria-hidden="true"></i> 1 phút trước</p>
+                                </div>
+                            </a>
+                            @for($i = 1; $i <= 10; $i++)
+                                <a class="content" href="#">
+                                    <div class="notification-item">
+                                        <h4 class="item-title">Hợp đồng số 00{{ $i }} đã được phê duyệt bởi <b>Phạm Kỳ Khôi</b></h4>
+                                        <p><i class="fa fa-clock-o" aria-hidden="true"></i> 1 phút trước</p>
+                                    </div>
+                                </a>
+                            @endfor
+                        </div>
+                        <li class="divider"></li>
+
+                        <div class="notification-footer text-center">
+                            <h4 class="menu-title">
+                                <a href='#' title='Xem tất cả'>Xem tất cả</a>
+                            </h4>
+                        </div>
                     </ul>
                 </li>
 
@@ -74,17 +117,53 @@
             <ul>
                 <li class="node-system">
                     <a href="javascript:void(0)" class="active">
-                        Danh mục
-                        <i class='glyphicon glyphicon-random'></i></a>
+                        <i class='glyphicon glyphicon-th-large'></i>
+                        Thông tin chung
+                    </a>
                     <ul class="sub-node-system">
                         <li>
+                            <a href="{{ route('locations.index') }}"  title="Quản lý khu vực">
+                            <i class="glyphicon glyphicon-pushpin"></i>&nbsp; Quản lý địa điểm</a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('branches.index') }}">
+                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý chi nhánh
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('rooms.index') }}">
+                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý phòng ban
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('positions.index') }}">
+                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý chức vụ
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('warehouses.index') }}">
+                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý kho
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('units.index') }}">
+                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý đơn vị tính
+                            </a>
+                        </li>
+
+                        <li>
                             <a href="{{ route('bills.index') }}">
-                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý chứng từ
+                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý bán hàng
                             </a>
                         </li>
                         <li>
                             <a href="{{ route('products.index') }}">
-                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý hàng hóa vật tư
+                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý mua hàng
                             </a>
                         </li>
                         <li>
@@ -97,11 +176,7 @@
                                 <i class="glyphicon glyphicon-pushpin"></i> Quản lý nhóm khách hàng
                             </a>
                         </li>
-                        <li>
-                            <a href="{{ route('units.index') }}">
-                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý đơn vị tính
-                            </a>
-                        </li>
+                        
                         <li>
                             <a href="#">
                                 <i class="glyphicon glyphicon-pushpin"></i> Quản lý mã vạch
@@ -117,30 +192,7 @@
                                 <i class="glyphicon glyphicon-pushpin"></i> Quản lý nhà cung cấp
                             </a>
                         </li>
-                        <li>
-                            <a href="{{ route('warehouses.index') }}">
-                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý kho
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('branches.index') }}">
-                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý chi nhánh
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('positions.index') }}">
-                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý chức vụ
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('rooms.index') }}">
-                                <i class="glyphicon glyphicon-pushpin"></i> Quản lý phòng ban
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('locations.index') }}"  title="Quản lý khu vực">
-                            <i class="glyphicon glyphicon-pushpin"></i>&nbsp; Quản lý địa điểm</a>
-                        </li>
+                        
                         <li>
                             <a href="{{ url('bills/symmetrical') }}"  title="Cân đối hàng tồn">
                             <i class="glyphicon glyphicon-pushpin"></i>&nbsp; Cân đối hàng tồn</a>
@@ -148,40 +200,87 @@
                     </ul>
                 </li>
 
-                <li>
-                    <a href="{{ url('bills/sale') }}"  title="Hóa đơn bán hàng">
-                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Hóa đơn bán hàng</a>
+                <li class="node-system">
+                    <a href="javascript:void(0)" title="Bán hàng phải thu">
+                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Bán hàng phải thu</a>
+                    <ul class="sub-node-system">
+                        <li>
+                            <a href="{{ route('quotations.create') }}"  title="Lập báo giá">
+                            <i class="glyphicon glyphicon-pushpin"></i>&nbsp; Lập báo giá</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="node-system">
+                    <a href="javascript:void(0)" title="Mua hàng phải trả">
+                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Mua hàng phải trả</a>
+                    <ul class="sub-node-system">
+                        <li>
+                            <a href="{{ route('locations.index') }}"  title="Phiếu yêu cầu mua hàng">
+                            <i class="glyphicon glyphicon-pushpin"></i>&nbsp; Phiếu yêu cầu mua hàng</a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('locations.index') }}"  title="Kế hoạch mua hàng">
+                            <i class="glyphicon glyphicon-pushpin"></i>&nbsp; Kế hoạch mua hàng</a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('locations.index') }}"  title="Đơn mua hàng">
+                            <i class="glyphicon glyphicon-pushpin"></i>&nbsp; Đơn mua hàng</a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('locations.index') }}"  title="Hợp đồng nhà cung cấp">
+                            <i class="glyphicon glyphicon-pushpin"></i>&nbsp; Hợp đồng nhà cung cấp</a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('locations.index') }}"  title="Hợp đồng nhà cung cấp">
+                            <i class="glyphicon glyphicon-pushpin"></i>&nbsp; Tiến độ đơn hàng, hợp đồng</a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('locations.index') }}"  title="Đề nghị thanh toán">
+                            <i class="glyphicon glyphicon-pushpin"></i>&nbsp; Đề nghị thanh toán</a>
+                        </li>
+                    </ul>
                 </li>
 
                 <li>
-                    <a href="{{ url('bills/wholesale') }}"  title="Bán buôn - Bán sỉ">
-                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Bán buôn - Bán sỉ</a>
+                    <a href="{{ url('bills/buy') }}"  title="Quản lý kho">
+                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Quản lý kho</a>
                 </li>
 
                 <li>
-                    <a href="{{ url('bills/buy') }}"  title="Hóa đơn mua hàng">
-                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Hóa đơn mua hàng</a>
-                </li>
-
-                <li>
-                    <a href="{{ url('bills/warehousetransfer') }}"  title="Chuyển kho nội bộ">
-                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Chuyển kho nội bộ</a>
+                    <a href="{{ url('bills/warehousetransfer') }}"  title="Chăm sóc khách hàng">
+                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Chăm sóc khách hàng</a>
+                    <ul class="sub-node-system">
+                        <li>
+                            <a href="{{ route('customers.index') }}"  title="Khách hàng">
+                            <i class="glyphicon glyphicon-user"></i>&nbsp; Khách hàng</a>
+                        </li>
+                    </ul>
                 </li>
                 <li>
                     <a href="{{ url('bills/destroystock') }}"  title="Phiếu hủy hàng">
-                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Phiếu hủy hàng</a>
+                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Nhân sự</a>
                 </li>
 
                 <li>
                     <a href="{{ url('bills/exportstock') }}"  title="Xuất hàng trả lại">
-                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Xuất hàng trả lại</a>
+                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Quản lý sản xuất</a>
                 </li>
 
                 <li>
                     <a href="{{ url('bills/importstock') }}"  title="Nhập hàng trả lại">
-                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Nhập hàng trả lại</a>
+                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Tài sản cố định</a>
                 </li>
 
+                <li>
+                    <a href="{{ url('bills/importstock') }}"  title="Nhập hàng trả lại">
+                    <i class="glyphicon glyphicon-th-large"></i>&nbsp; Kế toán</a>
+                </li>
             </ul>
         </nav>
     </div>
